1b47361fd1d7b175dfd381b8c5903ee69a2b3edb
bep3 <martin.curtet@gmail.com>
2023-03-16 19:47:34 -0400

    feat: register transaction
    - added transaction to register process to avoid data inconsistency
    - changed order of operations to accommodate transaction

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,33 +5,44 @@ exports.authController = {
   async register(req, res) {
     try {
       const { email, username, password, languageCode, emailPreferences } = req.body
-      const salt = await bcrypt.genSalt(10)
-      const bcryptPassword = await bcrypt.hash(password, salt)
 
-      const [user, created] = await db.user.findOrCreate({
-        where: { email: email },
-        defaults: {
-          username: username,
-          active: false
-        }
-      })
-
-      if (created) {
-        console.log(`User ${email} registered`)
-        return res.json({
-          status: 200,
-          message: `User registered`,
-          data: {
-            user: { id: user.id, email: user.email, username: user.username }
-          }
-        })
-      } else {
-        console.log(`User ${email} already exists`)
+      const existingUser = await db.user.findOne({ where: { email: email }})
+      if (existingUser) {
+        console.error(`User already exists`)
         return res.json({
           status: 409,
           message: `User already exists`
         })
       }
+
+      const result = await db.sequelize.transaction(async (t) => {
+        const user = await db.user.create({
+          email: email,
+          username: username,
+          active: false
+        }, { transaction: t })
+        const salt = await bcrypt.genSalt(10)
+        const bcryptPassword = await bcrypt.hash(password, salt)
+        await db.user_password.create({
+          user_id: user.id,
+          password: bcryptPassword
+        }, { transaction: t })
+        await db.user_preference.create({
+          user_id: user.id,
+          preferences: JSON.stringify(emailPreferences)
+        }, { transaction: t })
+        return user
+      })
+
+      console.log(`User ${email} registered`)
+      return res.json({
+        status: 200,
+        message: `User registered`,
+        data: {
+          user: { id: result.id, email: result.email, username: result.username }
+        }
+      })
+      
     } catch(err) {
       console.log(`Server error on /auth/register route`)
       console.log(err)
